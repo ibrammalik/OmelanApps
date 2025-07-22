65762a27620114fbb4cb936bb1d2b5929facc2a5
ibrammalik <malikmaul1173@gmail.com>
2025-07-22 09:23:35 +0700

    refactor: remove unused imports from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Page from './pages/dashboard/page';
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 import LandingPage from './pages/LandingPage';
@@ -10,7 +10,6 @@ import CaregiverOrderPage from './pages/OrderPage';
 import CaregiverDetailPage from './pages/CaregiverDetailPage';
 import OrderConfirmationPage from './pages/OrderConfirmationPage';
 import CaregiverListPage from './pages/CaregiverListPage';
-import LoaderScreen from './components/global/LoaderScreen';
 
 function App() {
   return (
