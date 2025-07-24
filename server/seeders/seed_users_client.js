1974fb61056214e3885748a303c89dd9aa7107b4
dieosurya <dieosurya@gmail.com>
2025-07-25 05:57:17 +0700

    [server] refactor (seed) : default data seed user

--- server/seeders/seed_users_client.js
+++ server/seeders/seed_users_client.js
@@ -6,36 +6,38 @@ module.exports = async (now) => {
   const hashedPassword = await bcrypt.hash("admin", 10);
 
   const adminClient = {
-    id: `client-admin`,
+    id: "client-admin",
     username: "admin@client.com",
     fullname: "admin client",
     password: hashedPassword,
     age: 40,
     address: "Jakarta",
-    biodata: "Admin Client Account",
+    biodata: "Admin client default",
     photo_url: faker.image.avatar(),
-    phone_number: "081234567890",
-    partner_name: "Pasangan Admin",
+    phone_number: "08123456789",
+    partner_name: "Admin Pasangan",
     emergency_contact: "081234567890",
     created_at: now,
     updated_at: now,
   };
 
-  const dummyClients = Array.from({ length: 6 }).map(() => ({
-    id: `client-${nanoid(10)}`,
-    username: faker.internet.email(),
-    fullname: faker.person.fullName(),
-    password: faker.internet.password(),
-    age: faker.number.int({ min: 30, max: 60 }),
-    address: faker.location.city(),
-    biodata: faker.lorem.paragraph(),
-    photo_url: faker.image.avatar(),
-    phone_number: faker.phone.number(),
-    partner_name: faker.person.fullName(),
-    emergency_contact: faker.phone.number(),
-    created_at: now,
-    updated_at: now,
-  }));
+  const dummyClients = await Promise.all(
+    Array.from({ length: 6 }).map(async () => ({
+      id: `client-${nanoid(10)}`,
+      username: faker.internet.email(),
+      fullname: faker.person.fullName(),
+      password: await bcrypt.hash("123456", 10), // random password juga di-hash
+      age: faker.number.int({ min: 30, max: 60 }),
+      address: faker.location.city(),
+      biodata: faker.lorem.paragraph(),
+      photo_url: faker.image.avatar(),
+      phone_number: faker.phone.number(),
+      partner_name: faker.person.fullName(),
+      emergency_contact: faker.phone.number(),
+      created_at: now,
+      updated_at: now,
+    }))
+  );
 
   return [adminClient, ...dummyClients];
 };
--- server/seeders/seed_users_partner.js
+++ server/seeders/seed_users_partner.js
@@ -6,13 +6,13 @@ module.exports = async (now) => {
   const hashedPassword = await bcrypt.hash("admin", 10);
 
   const adminPartner = {
-    id: `partner-admin`,
+    id: "partner-admin",
     username: "admin@partner.com",
     fullname: "admin partner",
     password: hashedPassword,
     age: 45,
     address: "Bandung",
-    biodata: "Admin Partner Account",
+    biodata: "Admin partner default",
     photo_url: faker.image.avatar(),
     phone_number: "082112341234",
     average_rating: 5.0,
@@ -22,22 +22,24 @@ module.exports = async (now) => {
     updated_at: now,
   };
 
-  const dummyPartners = Array.from({ length: 6 }).map(() => ({
-    id: `partner-${nanoid(10)}`,
-    username: faker.internet.email(),
-    fullname: faker.person.fullName(),
-    password: faker.internet.password(),
-    age: faker.number.int({ min: 30, max: 60 }),
-    address: faker.location.city(),
-    biodata: faker.lorem.sentences(),
-    photo_url: faker.image.avatar(),
-    phone_number: faker.phone.number(),
-    average_rating: faker.number.float({ min: 3.0, max: 5.0, precision: 0.1 }),
-    experience: faker.number.int({ min: 1, max: 10 }).toString(),
-    specialist: faker.person.jobTitle(),
-    created_at: now,
-    updated_at: now,
-  }));
+  const dummyPartners = await Promise.all(
+    Array.from({ length: 6 }).map(async () => ({
+      id: `partner-${nanoid(10)}`,
+      username: faker.internet.email(),
+      fullname: faker.person.fullName(),
+      password: await bcrypt.hash("123456", 10),
+      age: faker.number.int({ min: 30, max: 60 }),
+      address: faker.location.city(),
+      biodata: faker.lorem.paragraph(),
+      photo_url: faker.image.avatar(),
+      phone_number: faker.phone.number(),
+      average_rating: faker.number.float({ min: 3, max: 5, precision: 0.1 }),
+      experience: faker.number.int({ min: 1, max: 10 }).toString(),
+      specialist: faker.person.jobTitle(),
+      created_at: now,
+      updated_at: now,
+    }))
+  );
 
   return [adminPartner, ...dummyPartners];
 };
