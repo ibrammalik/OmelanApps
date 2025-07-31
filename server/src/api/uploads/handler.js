86e862d11546e68e7e8f59ddaa5c66a77139eb9f
VincentiusPramudito <vpramudito@yahoo.com>
2025-07-31 17:24:32 +0700

    update

--- server/src/api/uploads/handler.js
+++ server/src/api/uploads/handler.js
@@ -1,3 +1,5 @@
+const { badRequest } = require("@hapi/boom");
+
 class UploadsHandler {
   constructor(usersClientService, usersPartnerService, service, validator) {
     this._usersClientService = usersClientService;
@@ -14,14 +16,16 @@ class UploadsHandler {
     const userClient = await this._usersClientService.getUserDetailsById(credentialId);
     const userPartner = await this._usersPartnerService.getUserDetailsById(credentialId);
 
+    if (!userClient && !userPartner) {
+      throw badRequest('Failed upload photo: user not found.');
+    }
+
     this._validator.validateImageHeaders(photo.hapi.headers);
 
     const filelocation = await this._service.writeFile(photo, photo.hapi);
 
-    if (userClient)
-      await this._usersClientService.addPhotoUrl(credentialId, `${filelocation}`);
-    else if (userPartner)
-      await this._usersPartnerService.addPhotoUrl(credentialId, `${filelocation}`);
+    if (userClient) await this._usersClientService.addPhotoUrl(credentialId, `${filelocation}`);
+    else if (userPartner) await this._usersPartnerService.addPhotoUrl(credentialId, `${filelocation}`);
 
     const response = h.response({
       status: 'success',
