9c30e7f44705aa705c70ade9c378b62e1ed920ed
VincentiusPramudito <vpramudito@yahoo.com>
2025-07-31 19:02:40 +0700

    create notification migration for all user

---
+++ server/migrations/1753962712374_create-table-notification-client.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('notifications_client', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_client_id: {
+      type: 'VARCHAR(50)',
+      references: 'users_client(id)',
+      onDelete: 'CASCADE',
+    },
+    subject: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    status_read: {
+      type: 'VARCHAR(1)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('notifications_client');
+};
---
+++ server/migrations/1753962868739_create-table-notification-partner.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('notifications_partner', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_partner_id: {
+      type: 'VARCHAR(50)',
+      references: 'users_partner(id)',
+      onDelete: 'CASCADE',
+    },
+    subject: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    status_read: {
+      type: 'VARCHAR(1)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('notifications_partner');
+};
